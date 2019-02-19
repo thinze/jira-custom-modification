9f7303d89069bcca1b01b81a608aca88e1bd29fc
EifelDriver <eifel.driver@googlemail.com>
2019-02-19 10:51:28 +0100

    Implement the gadget update mechanism after modify gadget content (i.e. for search)

--- js_custom.js
+++ js_custom.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jira-custom-modification
 // @namespace    http://tampermonkey.net/
-// @version      0.36.0
+// @version      0.36.1
 // @description  add some additional features for JIRA
 // @author       T. Hinze
 // @match        https://positivmultimedia.atlassian.net/*
@@ -22,7 +22,7 @@
     var done_stati              = ['erledigt', 'geschlossen'];
 
     var action_css = '' +
-        '#my-jira-actions { position:fixed; background:rgba(0, 0, 0, 0.65); padding:5px; z-index:99999; top:25%; left:160px; ' +
+        '#my-jira-actions { position:fixed; background:rgba(0, 0, 0, 0.65); padding:5px; z-index:99999; top:300px; left:160px; ' +
         '  transform:translate(-50%, 0); }' +
         '#my-jira-actions h6 { color: #ccc; margin: -5px 0 5px; padding: 0; } ' +
         '#my-jira-actions .row {}' +
@@ -38,10 +38,10 @@
     '.overrun td, .overrun td a { color: red !important; } ' +
     '.todo-days-1 td, .todo-days-1 td a { color: orange !important; } ' +
     '.todo-days-2 td, .todo-days-2 td a { color: mediumaquamarine !important; } ' +
-    '#quick-search-field { display: inline-block; position: fixed; z-index: 99999; top: 30%; left: 160px; transform: translateX(-50%); ' +
+    '#quick-search-field { display: inline-block; position: fixed; z-index: 99999; top: 365px; left: 160px; transform: translateX(-50%); ' +
     '        width: 290px; max-width: 50%; background: #eee; padding: 2px; } ' +
     '#quick-search-field.filtering { background: lightgreen; } ' +
-    '#quick-search-clear { display: inline-block; position: fixed; width: 290px; max-width: 50%; top: 30%; left: 160px; transform: translateX(-50%); ' +
+    '#quick-search-clear { display: inline-block; position: fixed; width: 290px; max-width: 50%; top: 365px; left: 160px; transform: translateX(-50%); ' +
     '        height: 1em; background: darkred; margin-left: 1em; z-index: 99998; padding: 3px; } ' +
     '#quick-search-clear:hover { cursor: pointer; } ' +
     '#issuetable tr.tr-off, .gadget tr.tr-off { display: none !important; } ' +
@@ -72,32 +72,43 @@
         document.querySelector('head').appendChild(style);
     }
 
+    /**
+     * set multiple styles on an element
+     *
+     * @param elem      DOM node
+     * @param styles    key-value array {width: 'auto', height: 'auto', ... }
+     */
+    function setStylesOnElement(elem, styles){
+        Object.assign(elem.style, styles);
+    }
 
     // ===============  functions ==================
 
-
-    // exec modifications
-
-    // add project title to task view
-    var proj_title = document.querySelector('.css-6p2euf');
-    if (show_full_proj_title && proj_title) {
-        var bc = document.querySelector('.aui-nav-breadcrumbs');
-        if (bc) {
-            bc.prepend(proj_title.innerText + ' : ')
+    /**
+     * mark special projects (i.e. Update Service)
+     */
+    function markSpecialProjects() {
+        // add project title to task view
+        var proj_title = document.querySelector('.css-6p2euf');
+        if (show_full_proj_title && proj_title) {
+            var bc = document.querySelector('.aui-nav-breadcrumbs');
+            if (bc) {
+                bc.prepend(proj_title.innerText + ' : ')
+            }
         }
-    }
 
-    // mark service projects with color
-    if (mark_service_proj) {
-      var projects = document.querySelectorAll("td.project a");
-      if (projects.length) {
-        for (var idx=0; idx<projects.length; idx++) {
-          var prj = projects[idx];
-          if (prj.innerText.endsWith('Support Service')) {
-            prj.style.color = '#f00 !important';
-          }
+        // mark service projects with color
+        if (mark_service_proj) {
+            var projects = document.querySelectorAll("td.project a");
+            if (projects.length) {
+                for (var idx=0; idx<projects.length; idx++) {
+                    var prj = projects[idx];
+                    if (prj.innerText.endsWith('Support Service')) {
+                        prj.style.color = '#f00 !important';
+                    }
+                }
+            }
         }
-      }
     }
 
     function markOverrunedTasks() {
@@ -133,96 +144,111 @@
         );
     }
 
+    /**
+     * update gadgets height and width
+     *
+     * The trick is: minimize and maximize #page-body to force the gadget-update-method (of JIRA)
+     */
+    function updateGadgets() {
+        setStylesOnElement(document.querySelector('#page-body'), {width: '0px', minWidth: 'inherit'});
+        window.setTimeout(function() { // little bit delay before change styles again
+            setStylesOnElement(document.querySelector('#page-body'), {width: 'auto', minWidth: 'fit-content'});
+        }, 1);
+    }
+
     function addQuickSearch() {
+        var body    = document.querySelector('#page-body');
         var search  = document.createElement('INPUT');
         var clean   = document.createElement('SPAN');
         search.id   = 'quick-search-field';
         clean.id    = 'quick-search-clear';
-        document.querySelector('#page-body').appendChild(search);
-        document.querySelector('#page-body').appendChild(clean);
+        if (body) {
+            body.appendChild(search);
+            body.appendChild(clean);
 
-        search.addEventListener('keyup',
-            function (e) {
-                var field   = e.target;
-                var word    = field.value.toLowerCase();
-                if (word && word.length > 3) {
-                    // filter view
-                    field.className = field.className.replace(' filtering', '') + ' filtering';
-                    // filter gadgets
-                    var gadgets = document.querySelectorAll('div.gadget, #issuetable');
-                    gadgets.forEach(
-                        function (widget) {
-                            var rows = widget.querySelectorAll('tbody tr');
-                            rows.forEach(
-                                function (tr) {
-                                    if (tr.innerText.toLowerCase().indexOf(word) == -1) {
-                                        // invisible the tr
-                                        tr.className = tr.className.replace('tr-off', '') + ' tr-off';
-                                    } else {
-                                        tr.className = tr.className.replace('tr-off', '');
+            search.addEventListener('keyup',
+                function (e) {
+                    var field = e.target;
+                    var word = field.value.toLowerCase();
+                    if (word && word.length > 3) { // search if 4 or more chars given
+                        // filter view
+                        field.className = field.className.replace(' filtering', '') + ' filtering';
+                        // filter gadgets
+                        var gadgets = document.querySelectorAll('div.gadget, #issuetable');
+                        gadgets.forEach(
+                            function (widget) {
+                                var rows = widget.querySelectorAll('tbody tr');
+                                rows.forEach(
+                                    function (tr) {
+                                        if (tr.innerText.toLowerCase().indexOf(word) == -1) {
+                                            // invisible the tr
+                                            tr.className = tr.className.replace('tr-off', '') + ' tr-off';
+                                        } else {
+                                            tr.className = tr.className.replace('tr-off', '');
+                                        }
                                     }
-                                }
-                            )
-                            // resize widgets
-                            if (widget.tagName == 'DIV') {  // skip tables
-                                var results = widget.querySelector('div.results-wrap');
-                                var inline = widget.querySelector('.gadget-inline');
-                                if (results) {
-                                    if (widget.className.indexOf(' resized') == -1) {
-                                        widget.dataset.origHeight = inline.style.height;
-                                        widget.clsssName += ' resized';
+                                )
+                                // resize widgets
+                                if (widget.tagName == 'DIV') {  // skip tables
+                                    var results = widget.querySelector('div.results-wrap');
+                                    var inline = widget.querySelector('.gadget-inline');
+                                    if (results) {
+                                        if (widget.className.indexOf(' resized') == -1) {
+                                            widget.dataset.origHeight = inline.style.height;
+                                            widget.clsssName += ' resized';
+                                        }
+                                        inline.style.height = results.style.height;
                                     }
-                                    inline.style.height = results.style.height;
                                 }
                             }
+                        )
+                        updateGadgets();
+
+                    } else {
+                        // show all rows
+                        field.className = field.className.replace(' filtering', '');
+                        document.querySelectorAll('.gadget tr.tr-off, #issuetable tr.tr-off').forEach(
+                            function (tr) {
+                                tr.className = tr.className.replace('tr-off', '');
+                            }
+                        )
+                        // de-resize widgets
+                        updateGadgets();
+                        if (0) {
+                            var gadgets = document.querySelectorAll('div.gadget.resized');
+                            gadgets.forEach(
+                                function (widget) {
+                                    // resize widgets
+                                    var results = widget.querySelector('div.results-wrap');
+                                    var inline = widget.querySelector('.gadget-inline');
+                                    if (results) {
+                                        inline.style.height = widget.dataset.origHeight;
+                                        widget.clsssName = widget.className.replace(' resized', '');
+                                    }
+
+                                }
+                            );
                         }
-                    )
-                } else {
-                    // show all rows
+
+                    }
+                }
+            );
+            console.log('init');
+            clean.addEventListener('click',
+                function (e) {
+                    // clear quick-search-field and disable filtering
+                    var field = document.querySelector('#quick-search-field');
+                    field.value = '';
                     field.className = field.className.replace(' filtering', '');
                     document.querySelectorAll('.gadget tr.tr-off, #issuetable tr.tr-off').forEach(
                         function (tr) {
                             tr.className = tr.className.replace('tr-off', '');
                         }
-                    )
-                    // de-resize widgets
-                    var gadgets = document.querySelectorAll('div.gadget.resized');
-                    gadgets.forEach(
-                        function (widget) {
-                            // resize widgets
-                            var results = widget.querySelector('div.results-wrap');
-                            var inline  = widget.querySelector('.gadget-inline');
-                            if (results) {
-                                inline.style.height = widget.dataset.origHeight;
-                                widget.clsssName = widget.className.replace(' resized', '');
-                            }
-
-                        }
                     );
-
+                    updateGadgets();
                 }
-            }
-        );
-        console.log('init');
-        clean.addEventListener('click',
-            function(e) {
-                // clear quick-search-field and disable filtering
-                var field   = document.querySelector('#quick-search-field');
-                field.value = '';
-                field.className = field.className.replace(' filtering', '');
-                document.querySelectorAll('.gadget tr.tr-off, #issuetable tr.tr-off').forEach(
-                    function (tr) {
-                        tr.className = tr.className.replace('tr-off', '');
-                    }
-                )
-            }
-        );
-        // update gadget height
-        document.querySelectorAll('.gadget-conatiner h3.dashboard-item-title').forEach(
-            function(item) {
-
-            }
-        )
+            );
+        }
     }
 
     function useAlwaysOldIssueView() {
@@ -238,13 +264,6 @@
                 // a.click();
             }
         }
-    }
-
-    /**
-     * exec some tasks after the page has loading finished
-     */
-    function pageLoadFinish() {
-        window.clearInterval(watcher1); // looking for old-issue-view-link
     }
 
     function addDashboardFoldActions() {
@@ -285,22 +304,27 @@
         window.resize();
     }
 
+    /**
+     * exec some tasks after the page has loading finished
+     */
+    function pageLoadFinish() {
+        window.clearInterval(watcher1); // remove watcher for old-issue-view-link
+    }
 
-    /* ---  init script --- */
-
+    // ---  init script ---
     function startScript() {
         insertCss(css);
         addDashboardFoldActions();
-        markOverrunedTasks();
         addQuickSearch();
-        // old:
+        markOverrunedTasks();
+        markSpecialProjects();
     }
 
-    // instant (DOM Ready)
+    // ---  instant (DOM Ready)   ---
     window.setTimeout(startScript, 1000);
     watcher1 = window.setInterval(useAlwaysOldIssueView, 100);
 
-    // window loaded
+    // ---  window loaded  ---
     window.addEventListener("load", function(event) {
         pageLoadFinish();
     });
